3e889ca184b47f30524376d4dce285e403404956
Ikechukwu Israel <dkingisreal@gmail.com>
2023-12-10 22:41:42 +0100

    2

--- myApp.js
+++ myApp.js
@@ -5,6 +5,17 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 let Person;
 
+let personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String],
+})
+
+Person = mongoose.model("Person", personSchema)
+
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
